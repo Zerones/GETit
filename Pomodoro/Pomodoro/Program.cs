9d3afa02d8e48de0029beca11066ec7bb4622be6
Brage <Brage@getacademy.no>
2019-05-07 08:23:24 +0200

    4

---
+++ Pomodoro/Pomodoro/Program.cs
@@ -0,0 +1,32 @@
+﻿using PomodoroEngine;
+using System;
+
+namespace Pomodoro
+{
+    class Program : INotifyObject
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            Console.ReadLine();
+        }
+
+        public Program()
+        {
+            var pomodoro = new PomodoroEngine.Pomodoro(this, 1);
+            pomodoro.Start();
+        }
+
+        public void Tick(int minutes, int seconds)
+        {
+            Console.Clear();
+            if(seconds <= 0 && minutes <= 0)
+            {
+                Console.WriteLine("Ta en pause!");
+                Environment.Exit(0);
+            }
+            Console.WriteLine(minutes + ":" + seconds.ToString("00"));
+        }
+
+    }
+}
